5b3890b837c7df655741722ec07393dcb7423e2c
Shyaka Chaste <liochastej@gmail.com>
2024-10-31 07:11:22 +0200

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="">Evenara</h1>
-      <Button variant="destructive" className="">Hello</Button>
+      
     </main>
   );
 }
